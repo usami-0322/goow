2ca0c7f4d5c55c4a64841cd6bff1062903574184
usami-0322 <usaco0322@gmail.com>
2020-05-12 15:44:52 +0900

    update ログインの際にemail使用不要設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :employee_number, presence: true, uniqueness: true
+
+# 登録時にメールアドレス不要にする
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
+
