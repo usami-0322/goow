013685ef57f4057b53334942f4aa2ed8d198944f
usami-0322 <usaco0322@gmail.com>
2020-05-15 09:42:00 +0900

    update user編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show, :edit, :update]
   def index
     @users = User.all
   end
@@ -10,4 +11,24 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    if params[:user][:password].empty?
+      @user.errors.add(:password,:blank)
+      render 'edit'
+    elsif @user.update_with_password(user_params)
+      flash[:notise] = "パスワードを変更しました"
+      bypass_sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation, :current_password)
+    end
 end
