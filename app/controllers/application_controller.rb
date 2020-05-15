1614f80ac1939abc1c7afa11967188eb255fdcce
usami-0322 <usaco0322@gmail.com>
2020-05-15 09:41:32 +0900

    update protected2重表記のため削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     sign_up_params = [:name, :employee_number, :password, :password_confirmation]
     sign_in_params = [:employee_number, :password, :remember_me]
-    account_update = [:name, :area, ]
+    account_update = [:name, :area]
     # account_update, sign_in, sign_up, のフィールドを再定義
     devise_parameter_sanitizer.permit(:sign_up, keys: sign_up_params)
     devise_parameter_sanitizer.permit(:sign_in, keys: sign_in_params)
     devise_parameter_sanitizer.permit(:account_update, keys: account_update)
   end
 
-  protected
-
-  # loginしたらhome画面へリダイレクトし、indexアクションを実行する
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
