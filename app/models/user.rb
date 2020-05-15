1d0521b910c31de12088d4ffd32f548664aea368
usami-0322 <usaco0322@gmail.com>
2020-05-15 09:44:22 +0900

    update パスワードなしで編集するため

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :employee_number, presence: true, uniqueness: true
 
-# 登録時にメールアドレス不要にする
+  # 登録時にメールアドレス不要にする
   def email_required?
     false
   end
@@ -19,5 +19,17 @@ class User < ApplicationRecord
   def will_save_change_to_email?
     false
   end
-end
 
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+end
