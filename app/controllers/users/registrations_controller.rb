cd0d84f2231a7373fa4153c6d2e61b50c9387d9f
usami-0322 <usaco0322@gmail.com>
2020-05-15 09:42:59 +0900

    update　パスワードなしで編集できるよう修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -63,5 +63,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after update.
   def after_update_path_for(resource)
     user_path(resource)
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
   end
 end
