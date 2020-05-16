0c08d9f8d13df492e0fa40fe9d8c817b77231294
usami-0322 <usaco0322@gmail.com>
2020-05-16 22:03:10 +0900

    update carrier_waveコメントアウト

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider              => 'AWS',
-      :region                => 'ap-northeast-1',
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
-    }
-    config.fog_directory     =  'goowapp'
-  end
-end
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       :provider              => 'AWS',
+#       :region                => 'ap-northeast-1',
+#       :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+#       :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+#     }
+#     config.fog_directory     =  'goowapp'
+#   end
+# end
